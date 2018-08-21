38e0dc39bb0692b536ef647833e8ee1c9770da41
mperry <martin.perry@patientco.com>
2018-08-21 16:40:49 -0400

    added endpoint and query

--- billconfig.rb
+++ billconfig.rb
@@ -1,10 +1,35 @@
 require 'sinatra'
+require 'mongoid'
+
+Mongoid.load! "mongoid.config"
+
+get '/health' do
+  true
+end
+
+get '/configurations' do
+  Configuration.all.to_json
+end
+
 get '/configuration' do
-  configuration = Configuration. new(["address","name","balance"])
+  configurations = Configuration.all
+
+  [:providerid].each do |filter|
+    configurations = Configuration.send(filter, params[filter]) if params[filter]
+  end
+
+  configurations.to_json
 end
 
 class Configuration
-  def initialize(headers)
-    @headers = headers
-  end
+  include Mongoid::Document
+
+  field :providerid, type: String
+  field :headers, type: String
+  validates :providerid, presence: true
+  validates :headers, presence: true
+
+  index({ providerid: 1}, {unique: true, name: "proder_index"})
+
+  scope :providerid, -> (providerid) { where(providerid: providerid) }
 end
